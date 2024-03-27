fcd08923720ba77dd5bb0b1e9472072bf86591cb
Willem van Asperen <willem.van.asperen@paconsulting.com>
2024-03-27 19:58:25 +0000

    working with sunny day scenario

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from jinja2 import Template
 from loguru import logger
 from statemachine import State, StateMachine
 from pydantic import BaseModel, Field
@@ -60,24 +61,62 @@ class WorkOrderStateMachine(StateMachine):
     )
 
     def __init__(self, word_order_record: WorkOrderRecord):
-        super(WorkOrderStateMachine, self).__init__()
         self.record = word_order_record
-        self.dialogue: list[tuple[str, str]] = [("ai", self.current_state_value)]
+        self.dialogue: list[tuple[str, str]] = list()  # [("ai", self.current_state_value.render())]
+        super(WorkOrderStateMachine, self).__init__()
 
     # EVENT HANDLERS
     def on_user_input(self, *args, **kwargs):
-        logger.info(f"User input received: {args} and {kwargs}")
+        """
+        This hook gets triggered when a "user_input" event is received. It adds the user input to the dialogue
+        list (as the tuple ("user", user_input)) and triggers the AI background process.
+
+        The state machine will then progress into the next state which will be a "waiting for AI" state.
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        logger.debug(f"User input received: {args} and {kwargs}")
         self.dialogue.append(("user", args[0]))
         # self.prompt = kwargs["target"].value.render(self.record.dict())
         # call the LLM to interpret the prompt
 
-    def on_ai_extraction(self, *args):
-        logger.info(f"AI extraction received: {args}")
-        self.dialogue.append(("ai", args[0]))
+    def on_ai_extraction(self, *args, target: State):
+        """
+        This hook get triggered when an "ai_extraction" event is received. It adds the AI response to the dialogue
+        list as the tuple ("ai-response", ai_response).
+
+        This hook then looks at the 'value' of the target state - which is assumed to be a Jinja template - and renders
+        that template using the values of `self.record` and the template variable `ai_response` with the raw
+        response that is received from the AI. The rendering of that template is then added to the dialogue
+        list as the tuple ("ai", ai_chat_response).
+
+        The state machine will then transfer into the next state which will be a "waiting for user input" state.
+
+        :param args:
+        :param target: the `State` that the state machine will move into after this event.
+        :return:
+        """
+        logger.debug(f"AI extraction received: {args}")
+        ai_response = args[0]
+        self.dialogue.append(("ai-response", ai_response))
+
+        if isinstance(target.value, Template):
+            template_values = self.record.dict()
+            template_values["ai_response"] = ai_response
+            ai_chat_response = target.value.render(template_values)
+            self.dialogue.append(("ai", ai_chat_response))
+
+    def on_enter_state(self):
+        if len(self.dialogue) == 0:
+            logger.info("START")
+        else:
+            logger.info(f">> {self.dialogue[-1][0].upper()}: {self.dialogue[-1][1]}")
 
     # VALIDATIONS AND CONDITIONS
     def is_user_entry_valid(self, event_data: EventData):
-        logger.info("is user entry valid", event_data)
+        logger.debug("is user entry valid", event_data)
         return event_data.args is not None and len(event_data.args) > 0 and event_data.args[0] != ""
 
     def is_valid_ai_response(self, event_data: EventData):
@@ -91,19 +130,19 @@ class WorkOrderStateMachine(StateMachine):
             return ai_response.split(" ")[1] == target_activity_type
 
     def is_activity_type_leak(self, event_data: EventData):
-        logger.info("is activity_type 'leak'")
+        logger.debug("is activity_type 'leak'")
         return self._is_activity_type(event_data.args[0], "leak")
 
     def is_activity_type_paint(self, event_data: EventData):
-        logger.info("is activity_type 'paint'")
+        logger.debug("is activity_type 'paint'")
         return self._is_activity_type(event_data.args[0], "paint")
 
     # DATA EXTRACTORS
     def on_exit_user_enters_work_order(self, user_input):
-        logger.info("on exit user enters work order", user_input)
+        logger.debug("on exit user enters work order", user_input)
         self.record.work_order_summary = user_input
 
-    def on_enter_ai_extracts_activity_type(self, ai_response: str):
+    def on_exit_ai_extracts_activity_type(self, ai_response: str):
         self.record.activity_type = ai_response
         return self.record.activity_type
 
@@ -118,15 +157,15 @@ if __name__ == "__main__":
     dot = graph()
     dot.write_png("work-order-state-machine.png")
 
-    logger.info(f"BOT: {sm.current_state.value.render(sm.record.dict())}")
+    # logger.debug(f"BOT: {sm.current_state.value.render(sm.record.dict())}")
     sm.user_input("I repaired a leak")
+    logger.debug(sm)
 
-    logger.info(sm)
     sm.ai_extraction(ai.extract_activity_type(sm.dialogue[-1][1]))
-    logger.info(sm)
+    logger.debug(sm)
 
     sm.user_input("yes")
-    logger.info(sm)
+    logger.debug(sm)
 
     sm.ai_extraction(ai.extract_activity_type_verification(sm.dialogue[-1][1]))
-    logger.info(sm)
+    logger.debug(sm)
