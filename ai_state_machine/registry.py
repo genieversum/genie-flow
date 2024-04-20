6c37d75699ef4fd663238ad7183a642c60f17d90
Willem van Asperen <willem@van.asperen.org>
2024-04-20 19:15:01 +0200

    cleanup

--- ai_state_machine/registry.py
+++ ai_state_machine/registry.py
@@ -1,6 +1,5 @@
 from ai_state_machine.genie_state_machine import GenieModel
-from ai_state_machine.store import get_fully_qualified_name_from_class, \
-    get_class_from_fully_qualified_name
+
 
 _REGISTRY: dict[str, type[GenieModel]] = dict()
 
@@ -9,10 +8,10 @@ def register(model_key: str, cls: type[GenieModel]):
     assert isinstance(model_key, str)
     assert issubclass(cls, GenieModel)
 
-    _REGISTRY[model_key] = cls  # get_fully_qualified_name_from_class(cls)
+    _REGISTRY[model_key] = cls
 
 
 def retrieve(model_key: str) -> type[GenieModel]:
     assert isinstance(model_key, str)
 
-    return _REGISTRY[model_key]  # get_class_from_fully_qualified_name(_REGISTRY[model_key])
+    return _REGISTRY[model_key]
